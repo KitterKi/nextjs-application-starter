ef1a07b23cb146cf0d2f85a216f407e5b891ba89
KitterKi <125606954+KitterKi@users.noreply.github.com>
2025-07-09 10:55:20 -0400

    Add .next/static/chunks/src_app_activity_page_tsx_abf1e175._.js 1752072922220

---
+++ .next/static/chunks/src_app_activity_page_tsx_abf1e175._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8c850185._.js",
+  "static/chunks/src_7bf6a952._.js"
+],
+    source: "dynamic"
+});
