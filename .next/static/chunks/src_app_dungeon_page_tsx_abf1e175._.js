6a110bff9ce9a92e4bb455debe157233175630f9
KitterKi <125606954+KitterKi@users.noreply.github.com>
2025-07-09 10:55:26 -0400

    Add .next/static/chunks/src_app_dungeon_page_tsx_abf1e175._.js 1752072927585

---
+++ .next/static/chunks/src_app_dungeon_page_tsx_abf1e175._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d8e5fa12._.js",
+  "static/chunks/node_modules_5f3bca22._.js"
+],
+    source: "dynamic"
+});
